63da635884547f1da0abab5efbc0a83389a4c89d
Dennis Conetta <dconetta1@my.uri.edu>
2021-05-06 10:14:25 -0400

    final comments and edits

--- Exam4.py
+++ Exam4.py
@@ -12,7 +12,7 @@ def  poss_kmer(k, string):
     k: kmer length
     string: string of characters 
     
-    return: possible number of character to the k or number of substrings that
+    return: possible number of characters to the k or number of substrings that
     would fit in the string
      
     '''
@@ -24,7 +24,7 @@ def  poss_kmer(k, string):
     else:
         return poss_b
 
-# function to calculate k and observed kmers given a string of characters [ATCG]        
+# function to calculate k and observed kmers given a string of characters [ATCG] - also known as the actual kmers       
 def obs_kmer(k, string): 
     ''' 
     This is a function to caluclate k and observed kmers given a string of characters
@@ -35,6 +35,94 @@ def obs_kmer(k, string):
     
     return: possible combinations of substrings (given k)
      
+    '''
+    full_list = [] # create empty data frame with duplicates
+    unique_list = [] # create empty data frame for unique combinations
+    count = 0 # create empty counter 
+    string_l = len(string) # calculate length of the string as done above 
+    for i in range(1,string_l+1): # indexing with i across all kmer values
+        single_k = string[(i-1):(i-1+k)] # defining the range of k values [1:length(string)] 
+        if len(single_k) == k: # only include kmers of the length we're looking at 
+            full_list.append(single_k) # add all kmers to a df 
+    for item in full_list:
+        if item not in unique_list: # select unique combinations
+            count += 1 # add 1 to running count every time there is a new combination
+            unique_list.append(item)
+    return(count) # this is the total number of unquie combinations of k (1:9) letters
+
+# function to create a data frame that includes columns for k, possible, and observed kmers given a string of characters [ATCG]        
+def make_df(string): 
+    ''' 
+    This is a function to create table of possible, and observed kmers given a string of characters
+    for exmaple: ATTGGCATT
+    
+    string: string of characters 
+    
+    return: table with totals that equal linguistic complexity 
+     
+    '''
+    df_empty = [] # create empty data frame
+    string_l = len(string)
+    for k in range(1,string_l+1):
+        df_output.append([obs_kmer(k, string), poss_kmer(k, string)])
+    df_output = pd.DataFrame(df_output, index = range(1, string_l+1), columns =  ["observed kmers", "possible kmers"])
+    df_output.loc['Total'] = df_output.sum()
+    return(df_output)
+
+# function to caluclate linguistic complexity (ratio of total observed and total possible - basically how close is the observed to all potential outcomes)
+def linguistic_complexity(df_output):
+    ''' 
+    This calculates the linguistic complexity (total obs/ total poss)
+    ''' 
+    ling_complex = df_output.loc['Total', 'Observed kmers'] / df_output.loc['Total', 'Possible kmers']
+    return(ling_complex)
+
+
+
+# function to output file
+def main(args):
+    '''
+    This takes a file (.csv) of strings and outputs
+    a new file with the table of k, possible, observed and linguistic complexity
+
+    '''
+# Establish errors
+    assert args.k > 0
+    if (args.k > 0): # making sure that k values have to all be greater than zero for functions to run/pass test
+        print(args.k)
+    else: print("NA")
+    
+    assert args.k <= args.string_l
+    if (args.k <= args.string_l): #making sure that k values that exceed the length of the string do not keep creating outputs since it is not possible
+        print(args.k)
+    else: print("NA")
+    
+    assert string == char_list["A", "C", "T", "G"]
+    assert matched_list = [characters in char_list for characters in string])
+    if (string_contains_chars = all(matched_list)): #making sure that only characters A, C, T, and G are recognized as valid string characters
+        print(string)
+    else: print("NA")
+    
+#test that we can read in file
+
+#this is where functions will go 
+    seq_test = open("seq_test.txt") # read in sequence example text file
+    print(seq_test.read()) #output at command line to see that it is working
+    data = seq_test #defining new data frame with sequence data that is provided
+    m_df = make_df(data) # generate data.frame with k, obs_kmer, and poss_kmer values (df_output)
+    ling = linguistic_complexity(data) # calculate ling complexity of each string in sequence file
+    m_df.to_csv('output') #write output to csv
+
+
+# this is for test script (this reference main function)
+if __name__ == '__main__':    #for all function called main do the following
+   parser = argparse.ArgumentParser() #use the argparse function to separate out strings
+   parser.add_argument(seq_test, type=argparse.FileType('r')) #specifying what files to parse into separate strings
+   args = parser.parse_args() #defining args as separated string file
+   main(args) #running args through the main function
+
+
+
     '''
     full_list = [] # create empty data frame with duplicates
     unique_list = [] # create empty data frame for unique combinations
--- Exam4_test.py
+++ Exam4_test.py
@@ -1,52 +1,80 @@
 # python3 string_script.py py.test
+# setting tests for Exam4 python script to purposefully pass and fail to make sure functions are legitimate
 
-#test multiple inputs (e.g. large k or small k)
-
-from string_script import *
+from Exam4 import *
 
 import pandas as pd 
 
-# test obs kmer function 1
+# Observed Kmer Function Test 1
+# test when K value is 1 for given string 'ATTTGGATT'
 def test_obs_kmers_1():
   k = 1
   string = 'ATTTGGATT'
   actual_result = obs_kmers(k, string)
-  expected_result = 3
+  expected_result = 3 #calculated from obs_kmer function
   assert actual_result == expected_result
   
-  # test obs kmer function 2
+# Observed Kmer Function Test 2
+# test when K value is 0 for given string 'ATTTGGATT'
+  # k should not run because in script told that all k values should be greater than 0 (NA should be output)
 def test_obs_kmers_1():
   k = 0
   string = 'ATTTGGATT'
   actual_result = obs_kmers(k, string)
-  expected_result = 
+  expected_result = NA
+  assert actual_result == expected_result
+  
+# Observed Kmer Function Test 3
+# test when K value is 100 for given string 'ATTTGGATT'
+  # k should not run because in script it was set that that k values cannot exceed the length of the string
+def test_obs_kmers_1():
+  k = 100
+  string = 'ATTTGGATT'
+  actual_result = obs_kmers(k, string)
+  expected_result = NA
   assert actual_result == expected_result
 
-# test poss kmer function
+# Observed Kmer Function Test 4
+# test when K value is 1 for given string 'ATTTGGAKX'
+  # k should not run because in script made it so that characters outside of A, C, T, and G were not to be included
+def test_obs_kmers_1():
+  k = 1
+  string = 'ATTTGGAKX'
+  actual_result = obs_kmers(k, string)
+  expected_result = NA
+  assert actual_result == expected_result
+  
+#Possible Kmer Function Test 1 
+# when K value is 1 for given string 'ATTTGGATT'
 def test_poss_kmer():
   k = 1
+  string = 'ATTTGGATT'
   actual_result = poss_kmers(k, string)
   expected_result = 4
   assert actual_result == expected_result
 
-# look at obs/poss for letters 
-# test the making of the dataframe (df_output)
-# cant ref variables from not function 
+
+# Making Data Frame Function Test 1 
+# when K value is for given string 'ATT'
+  #brackets indicate the list of observed and possible kmer values and including the total row
 def test_df_output1():
   string = 'ATT'
-  df_output_exp = [[2,3], [2,2], [1,1],[5,6]] # this is lst of list, make data.frame (check for functions)
+  df_output_exp = {'observed kmers': [2,2,1,5], 
+            'possible kmers': [3,2,1,6]
+            }
   df_actual = make_df(string)
-  result =  df_exp.equals(df_actual) #also try compare (this is an output, its a dataframe)
-  assert result == True
+  test = pd.DataFrame(df_output)
+  final =  df_output_exp.equals(df_actual) #also try compare (this is an output, its a dataframe)
+  assert final == True
 
 
-# next make test for linguistic function
+# Linguistic Complexity Function Test 1 
+# when K value is for given string 'ATT'
 
-
-
-
-
-
-
-13
-
+def test_ling_complex1():
+  df_output_exp = {'observed kmers': [2,2,1,5], 
+            'possible kmers': [3,2,1,6],
+            index = [1,2,3, 'total]}
+  actual_result = linguistic_complexity(df_output_exp)
+  expected_result = 0.83
+  assert == actual_result == expected_result
